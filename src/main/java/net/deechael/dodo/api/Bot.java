a83235a58aabcb44db5dc4341f9469e899aa91df
deechael <deechael@outlook.com>
2022-08-24 23:16:47 +0800

    Added javadoc comments to Bot class

--- src/main/java/net/deechael/dodo/api/Bot.java
+++ src/main/java/net/deechael/dodo/api/Bot.java
@@ -5,16 +5,39 @@ import net.deechael.dodo.event.Listener;
 
 public interface Bot {
 
+    /**
+     * Start the bot
+     */
     void start();
 
+    /**
+     * When run the task after the websocket has started
+     * @param runnable task
+     */
     void runAfter(Runnable runnable);
 
+    /**
+     * Add an event listener
+     * @param listener event listener
+     */
     void addEventListener(Listener listener);
 
+    /**
+     * Unregister an event listener
+     * @param listener event listener
+     */
     void unregisterEventListener(Listener listener);
 
+    /**
+     * Register a command
+     * @param command dodo command
+     */
     void registerCommand(DodoCommand command);
 
+    /**
+     * Get the client
+     * @return client
+     */
     Client getClient();
 
 }
